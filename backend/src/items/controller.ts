6d93f5c388c466162c15f8f3a1ea1c55b887eb3d
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2026-01-13 22:41:15 +0100

    updated backend

--- backend/src/items/controller.ts
+++ backend/src/items/controller.ts
@@ -1,17 +1,11 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
+// import type { ObjectId } from "mongoose";
 
+import type { ShoppingItem, ShoppingItemWrite, ShoppingItemRead } from "./model.js";
 
-interface ShoppingItem {
-	// _id: ObjectId;
-	_id: number;
-	name: string;
-	bought: boolean;
-	createdAt: Date;
-}
 
 const itemList: ShoppingItem[] = [];
-
 const router: Router = Router();
 
 
@@ -19,14 +13,11 @@ router.get(
 
     "/",
 
-    async (request: Request, response: Response): Promise<void> => {
+    async (request: Request, response: Response<ShoppingItemRead[]>): Promise<void> => {
 
-        const item: ShoppingItem = {
-            _id: 33,
-            name: "ssssssssss",
-            bought: true,
-            createdAt: new Date()
-        };
+        // note: use mongodb to return item list
+
+        const list: ShoppingItemRead[] = itemList.map(item => { return { ...item, id: 44 } });
 
         response.status(200).json(itemList);
 
@@ -35,11 +26,56 @@ router.get(
 );
 
 
+router.get(
+
+    "/:id",
+
+    async (request: Request, response: Response<ShoppingItemRead | any>): Promise<void> => {
+
+        // note: replace this with db search
+
+        const { id } = request.params;
+        const result: ShoppingItem | undefined = itemList.find(item => item._id.toString() === id);
+
+        if (!result) {
+
+            console.log("item not found");
+            response.status(404).json({message: `item not found: ${id}`});
+
+            return;
+        }
+
+        else {
+
+            console.log("item found");
+            response.status(200).json(result);
+
+            return;
+        }
+    }
+);
+
+
 router.post(
 
     "/",
 
-    async (): Promise<void> => {
+    async (request: Request<any, any, ShoppingItemWrite>, response: Response<ShoppingItemRead>): Promise<void> => {
+
+        console.log(`creating item ${JSON.stringify(request.body)}`);
+
+        // read request body
+        // add it to database
+        // return added item
+
+        const item: ShoppingItemRead = {
+            ...request.body,
+            _id: 55,
+            createdAt: new Date()
+        };
+
+        response.status(200).json(item);
+
         return;
     }
 );
@@ -49,7 +85,19 @@ router.put(
 
     "/:id",
 
-    async (): Promise<void> => {
+    async (request: Request<any, any, ShoppingItemWrite>, response: Response<ShoppingItemRead>): Promise<void> => {
+
+        console.log(`updating item ${request.params.id} ${request.body}`);
+
+        const item: ShoppingItemRead = {
+            _id: 33,
+            name: "item1",
+            bought: true,
+            createdAt: new Date()
+        };
+
+        response.status(200).json(item);
+
         return;
     }
 );
@@ -59,7 +107,10 @@ router.delete(
 
     "/:id",
 
-    async (): Promise<void> => {
+    async (request: Request, response: Response): Promise<void> => {
+
+        console.log(`deleting item ${request.params.id}`);
+
         return;
     }
 );
