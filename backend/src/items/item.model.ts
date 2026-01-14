5daccce7300948785b37f5d117bc6d17fd18fb10
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2026-01-14 20:31:43 +0100

    renamed model.ts to item.model.ts

---
+++ backend/src/items/item.model.ts
@@ -0,0 +1,30 @@
+import z from "zod";
+import type { Model } from "mongoose";
+import { Schema, model, Document } from "mongoose";
+
+
+export interface ShoppingItem extends Document {
+    // _id: ObjectId;
+    name: string;
+    bought: boolean;
+    createdAt: Date;
+}
+
+const definition = {
+    name: { type: String, required: true },
+    bought: { type: Boolean, required: true },
+    createdAt: { type: Date, required: true, default: new Date() }
+};
+
+const validatorShape = {
+    name: z.string().min(1, "name cannot be empty").max(255, "name cannot be longer than 255 characters"),
+    bought: z.boolean()
+};
+
+const schema: Schema = new Schema(definition);
+
+export const ShoppingItemValidator = z.object(validatorShape);
+export const ShoppingItemModel: Model<ShoppingItem> = model<ShoppingItem>("ShoppingItem", schema);
+
+export type ShoppingItemWrite = z.infer<typeof ShoppingItemValidator>;
+export type ShoppingItemRead = Pick<ShoppingItem, "name" | "bought" | "createdAt"> & { id: string };
